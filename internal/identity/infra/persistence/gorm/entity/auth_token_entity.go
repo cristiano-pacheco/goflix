061592a670f4f5b22740140c9792fd51b14d1e47
Cristiano Pacheco <chris.spb25@gmail.com>
2025-05-25 18:15:27 -0300

    feat: add AuthTokenEntity and UserEntity models for authentication and user management

---
+++ internal/identity/infra/persistence/gorm/entity/auth_token_entity.go
@@ -0,0 +1,16 @@
+package entity
+
+import "time"
+
+type AuthTokenEntity struct {
+	ID        uint64    `gorm:"primarykey;autoIncrement;column:id"`
+	UserID    uint64    `gorm:"type:bigint;not null;column:user_id"`
+	Token     string    `gorm:"type:varchar;not null;unique;column:token"`
+	ExpiresAt time.Time `gorm:"type:timestamptz;not null;column:expires_at"`
+	CreatedAt time.Time `gorm:"type:timestamptz;default:now();column:created_at"`
+	UpdatedAt time.Time `gorm:"type:timestamptz;default:now();column:updated_at"`
+}
+
+func (*AuthTokenEntity) TableName() string {
+	return "login_tokens"
+}
---
+++ internal/identity/infra/persistence/gorm/entity/user_entity.go
@@ -0,0 +1,22 @@
+package entity
+
+import "time"
+
+type UserEntity struct {
+	ID                     uint64     `gorm:"primarykey;autoIncrement;column:id"`
+	Name                   string     `gorm:"type:varchar;not null;column:name"`
+	Email                  string     `gorm:"type:varchar;not null;unique;column:email"`
+	PasswordHash           string     `gorm:"type:varchar;not null;column:password_hash"`
+	IsActivated            bool       `gorm:"type:boolean;not null;default:false;column:is_activated"`
+	ConfirmationToken      *string    `gorm:"type:varchar;column:confirmation_token"`
+	ConfirmationExpiresAt  *time.Time `gorm:"type:timestamptz;column:confirmation_expires_at"`
+	ConfirmedAt            *time.Time `gorm:"type:timestamptz;column:confirmed_at"`
+	ResetPasswordToken     *string    `gorm:"type:varchar;column:reset_password_token"`
+	ResetPasswordExpiresAt *time.Time `gorm:"type:timestamptz;column:reset_password_expires_at"`
+	CreatedAt              time.Time  `gorm:"type:timestamptz;default:now();column:created_at"`
+	UpdatedAt              time.Time  `gorm:"type:timestamptz;default:now();column:updated_at"`
+}
+
+func (*UserEntity) TableName() string {
+	return "users"
+}
