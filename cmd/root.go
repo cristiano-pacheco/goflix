060314c56c87edef40d1823fee146842577198a9
Cristiano Pacheco <chris.spb25@gmail.com>
2025-05-25 18:23:14 -0300

    feat: integrate FX framework and initialize application with shared and identity modules

--- cmd/root.go
+++ cmd/root.go
@@ -4,20 +4,25 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"go.uber.org/fx"
+
+	"github.com/cristiano-pacheco/goflix/internal/identity"
+	shared_modules "github.com/cristiano-pacheco/goflix/internal/shared/modules"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "goflix",
 	Short: "GoFlix API",
 	Long:  `GoFlix API is a RESTful API for managing a video streaming platform.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		app := fx.New(
+			shared_modules.Module,
+			identity.Module,
+		)
+		app.Run()
+	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
