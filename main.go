4f4d302a1a4b20474dde3d79b6b6faae46820fc0
Cristiano Pacheco <chris.spb25@gmail.com>
2025-05-28 21:15:11 -0300

    refactor: remove unused context and log imports from main.go

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"context"
-	"log"
-
 	"github.com/cristiano-pacheco/goflix/cmd"
 	"github.com/cristiano-pacheco/goflix/internal/shared/modules/config"
 	"github.com/cristiano-pacheco/goflix/internal/shared/modules/otel"
@@ -21,15 +18,7 @@ import (
 // @BasePath  /
 func main() {
 	config.Init()
-
 	cfg := config.GetConfig()
 	otel.Init(cfg)
-
-	defer func() {
-		if err := otel.Trace().Shutdown(context.Background()); err != nil {
-			log.Printf("Error shutting down tracer provider: %v", err)
-		}
-	}()
-
 	cmd.Execute()
 }
